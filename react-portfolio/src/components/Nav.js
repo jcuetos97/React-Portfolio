43dc3da834286c193b84e251f78c4ab868ccbbbc
unknown <jcuetos97@gmail.com>
2022-10-27 09:35:27 -0500

    ignore node modules added

---
+++ react-portfolio/src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../css/Nav.css'
+
+export default function Nav() {
+
+    return (
+        <ul>
+            <li><a href="default.asp">Home</a></li>
+            <li><a href="news.asp">News</a></li>
+            <li><a href="contact.asp">Contact</a></li>
+            <li><a href="about.asp">About</a></li>
+        </ul>
+    );
+}
